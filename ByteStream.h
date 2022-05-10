a16f2180fa85e4e4cb287eb44bb2a84419b9197a
Adam Al-Hosam <adam.alhosam@icloud.com>
2022-05-10 21:51:04 +0200

    Serializer/unserializer seems to work (GUI <-> client) time for testing

--- ByteStream.h
+++ ByteStream.h
@@ -8,18 +8,18 @@
 #include <memory>
 #include <ostream>
 
-
 class ByteStream {
  private:
   std::vector<uint8_t> data;
   uint8_t ptr_to_act_el;
 
  public:
-  ByteStream() : data(256), ptr_to_act_el(0) {};
+  ByteStream() : data(256), ptr_to_act_el(0){};
   void resetPtr() {
     ptr_to_act_el = 0;
   }
-  ByteStream& operator<<(uint8_t& x) {
+
+  ByteStream& operator<<(uint8_t x) {
     data[ptr_to_act_el] = x;
     ptr_to_act_el += sizeof(x);
     return *this;
@@ -30,15 +30,41 @@ class ByteStream {
     return *this;
   }
 
-  ByteStream& operator<<(uint16_t& x) {
+  ByteStream& operator<<(uint16_t x) {
     x = htons(x);
     std::memcpy(&data[ptr_to_act_el], &x, sizeof(x));
     ptr_to_act_el += sizeof(x);
     return *this;
   }
   ByteStream& operator>>(uint16_t& x) {
-    std::memcpy( &x,&data[ptr_to_act_el], sizeof(x));
+    std::memcpy(&x, &data[ptr_to_act_el], sizeof(x));
     x = ntohs(x);
+    ptr_to_act_el += sizeof(x);
+    return *this;
+  }
+
+  ByteStream& operator<<(uint64_t x) {
+    x = htobe64(x);
+    std::memcpy(&data[ptr_to_act_el], &x, sizeof(x));
+    ptr_to_act_el += sizeof(x);
+    return *this;
+  }
+  ByteStream& operator>>(uint64_t& x) {
+    std::memcpy(&x, &data[ptr_to_act_el], sizeof(x));
+    x = be64toh(x);
+    ptr_to_act_el += sizeof(x);
+    return *this;
+  }
+
+  ByteStream& operator<<(uint32_t x) {
+    x = htonl(x);
+    std::memcpy(&data[ptr_to_act_el], &x, sizeof(x));
+    ptr_to_act_el += sizeof(x);
+    return *this;
+  }
+  ByteStream& operator>>(uint32_t& x) {
+    std::memcpy(&x, &data[ptr_to_act_el], sizeof(x));
+    x = ntohl(x);
     ptr_to_act_el += sizeof(x);
     return *this;
   }
@@ -48,7 +74,7 @@ class ByteStream {
     ptr_to_act_el += sizeof(x);
     return *this;
   }
-  ByteStream& operator<<(char& x) {
+  ByteStream& operator<<(char x) {
     data[ptr_to_act_el] = x;
     ptr_to_act_el += sizeof(x);
     return *this;
@@ -61,11 +87,66 @@ class ByteStream {
     ptr_to_act_el += s.length();
     return *this;
   }
-  ByteStream& operator<<(std::string& s) {
+  ByteStream& operator<<(std::string s) {
     uint8_t length = s.size();
     *this << length;
     std::memcpy(&data[ptr_to_act_el], s.data(), length);
     ptr_to_act_el += s.length();
+    return *this;
+  }
+
+  template <typename T>
+  ByteStream& operator>>(std::vector<T>& x) {
+    uint32_t len;
+    *this >> len;
+    x.resize(len);
+    for (int i = 0; i < len; ++i) {
+      *this >> x[i];
+    }
+    return *this;
+  }
+  template <typename T>
+  ByteStream& operator<<(std::vector<T> x) {
+    uint32_t len = x.size();
+    *this << len;
+    for (int i = 0; i < len; ++i) {
+      *this << x[i];
+    }
+    return *this;
+  }
+
+  template <typename T1, typename T2>
+  ByteStream& operator>>(std::map<T1, T2>& x) {
+    uint32_t len;
+    *this >> len;
+    x.clear();
+    std::pair<T1, T2> temp_val;
+    for (int i = 0; i < len; ++i) {
+      *this >> temp_val;
+      x.insert(temp_val);
+    }
+    return *this;
+  }
+  template <typename T1, typename T2>
+  ByteStream& operator<<(std::map<T1, T2> x) {
+    uint32_t len = x.size();
+    *this << len;
+    for (auto& it: x) {
+      *this << it;
+    }
+    return *this;
+  }
+
+  template <typename T1, typename T2>
+  ByteStream& operator>>(std::pair<T1, T2>& x) {
+    *this >> x.first;
+    *this >> x.second;
+    return *this;
+  }
+  template <typename T1, typename T2>
+  ByteStream& operator<<(std::pair<T1, T2> x) {
+    *this << x.first;
+    *this << x.second;
     return *this;
   }
 };
--- Messages.h
+++ Messages.h
@@ -8,11 +8,119 @@
 #include <map>
 #include <memory>
 #include <ostream>
+#include <utility>
 
 #include "ByteStream.h"
 
+std::ostream& operator<<(std::ostream& os, uint8_t x) {
+  os << (char) (x + '0');
+
+  return os;
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream& os, std::vector<T> v) {
+  for (auto k: v) {
+    os << k << '\n';
+  }
+
+  return os;
+}
+
+template <typename T1, typename T2>
+std::ostream& operator<<(std::ostream& os, std::map<T1, T2> v) {
+  for (auto [k, val]: v) {
+    os << "{" << k << " : " << val << "}" << '\n';
+  }
+
+  return os;
+}
+
+class Player {
+ private:
+ public:
+  std::string name;
+  std::string address;
+  Player(std::string name, std::string address)
+      : name(std::move(name)), address(std::move(address)){};
+  Player() : name(), address(){};
+  friend ByteStream& operator<<(ByteStream& os, Player& player) {
+    os << player.name;
+    os << player.address;
+    return os;
+  }
+
+  friend ByteStream& operator>>(ByteStream& os, Player& player) {
+    os >> player.name;
+    os >> player.address;
+    return os;
+  }
+
+  friend std::ostream& operator<<(std::ostream& os, Player& p) {
+    os << "{" << p.name << " : " << p.address << "}";
+    return os;
+  }
+};
+
+class Position {
+ private:
+ public:
+  uint16_t x{};
+  uint16_t y{};
+  std::string name;
+  std::string address;
+  Position(uint16_t x, uint16_t y) : x(x), y(y){};
+  Position() : name(), address(){};
+  friend ByteStream& operator<<(ByteStream& os, Position& position) {
+    os << position.x;
+    os << position.y;
+    return os;
+  }
+
+  friend ByteStream& operator>>(ByteStream& os, Position& position) {
+    os >> position.x;
+    os >> position.y;
+    return os;
+  }
+
+  friend std::ostream& operator<<(std::ostream& os, Position& p) {
+    os << "{" << p.x << " : " << p.y << "}";
+    return os;
+  }
+};
+
+class Bomb {
+ private:
+ public:
+  Position position;
+  uint16_t timer;
+
+  Bomb(Position pos, uint16_t timer) : position(std::move(pos)), timer(timer){};
+  Bomb() : position(), timer(){};
+  friend ByteStream& operator<<(ByteStream& os, Bomb& bomb) {
+    os << bomb.position;
+    os << bomb.timer;
+    return os;
+  }
+
+  friend ByteStream& operator>>(ByteStream& os, Bomb& bomb) {
+    os >> bomb.position;
+    os >> bomb.timer;
+    return os;
+  }
+
+  friend std::ostream& operator<<(std::ostream& os, Bomb& p) {
+    os << "{" << p.position << " : " << p.timer << "}";
+    return os;
+  }
+};
+
 class Message {
  public:
+  using PlayerId = uint8_t;
+  using BombId = uint32_t;
+  using Score = uint32_t;
+
   typedef std::shared_ptr<Message> (*MessageFactory)(ByteStream&);
   static std::map<char, MessageFactory>& message_map() {
     static auto* ans = new std::map<char, MessageFactory>();
@@ -136,6 +244,7 @@ class Lobby : public DrawMessage {
   uint16_t game_length{};
   uint16_t explosion_radius{};
   uint16_t bomb_timer{};
+  std::map<Message::PlayerId, Player> players;
 
  public:
   static std::shared_ptr<DrawMessage> create(ByteStream& rest) {
@@ -144,20 +253,50 @@ class Lobby : public DrawMessage {
   }
   explicit Lobby(ByteStream& stream) {
     stream >> server_name >> players_count >> size_x >> size_y >> game_length >>
-        explosion_radius >> bomb_timer;
+        explosion_radius >> bomb_timer >> players;
   };
   void say_hello() override {
-    std::cout << "I am Lobby";
+    std::cout
+       << " server_name: " << this->server_name
+       << " players_count: " << this->players_count
+       << " size_x: " << this->size_x << " size_y: " << this->size_y
+       << " game_length: " << this->game_length
+       << " explosion_radius: " << this->explosion_radius
+       << " bomb_timer: " << this->bomb_timer << " players: " << this->players;
   }
-  friend std::ostream& operator<<(std::ostream& os, const Lobby& lobby) {
-    os
-       << " server_name: " << lobby.server_name
-       << " players_count: " << lobby.players_count
-       << " size_x: " << lobby.size_x << " size_y: " << lobby.size_y
-       << " game_length: " << lobby.game_length
-       << " explosion_radius: " << lobby.explosion_radius
-       << " bomb_timer: " << lobby.bomb_timer;
-    return os;
+
+};
+
+class Game : public DrawMessage {
+ private:
+  std::string server_name;
+  uint16_t size_x{};
+  uint16_t size_y{};
+  uint16_t game_length{};
+  uint16_t turn{};
+  std::map<Message::PlayerId, Player> players;
+  std::map<PlayerId, Position> player_positions;
+  std::vector<Position> blocks;
+  std::vector<Bomb> bombs;
+  std::vector<Position> explosions;
+  std::map<PlayerId, Score> scores;
+
+ public:
+  static std::shared_ptr<DrawMessage> create(ByteStream& rest) {
+    return std::dynamic_pointer_cast<DrawMessage>(std::make_shared<Game>(rest));
+  }
+  explicit Game(ByteStream& stream) {
+    stream >> server_name >> size_x >> size_y >> game_length >> turn >>
+        players >> player_positions >> blocks >> bombs >> explosions >> scores;
+  };
+  void say_hello() override {
+    std::cout
+       << " server_name: " << this->server_name << " size_x: " << this->size_x
+       << " size_y: " << this->size_y << " game_length: " << this->game_length
+       << " turn: " << this->turn << " players: " << this->players
+       << " player_positions: " << this->player_positions
+       << " blocks: " << this->blocks << " bombs: " << this->bombs
+       << " explosions: " << this->explosions << " scores: " << this->scores;
   }
 };
 
